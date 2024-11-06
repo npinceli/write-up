0feff17b856c6ed11e61f5b33af9bb74796ff75b
npinceli <pincelinicolas@gmail.com>
2024-11-06 19:39:46 -0300

    feat: adiciona sugestoes de usuarios para seguir na view do feed

--- models/sql/sql_sel_user_info.sql
+++ models/sql/sql_sel_user_info.sql
@@ -9,7 +9,6 @@ FROM
 WHERE 
     <dtml-if user_id>
         id = <dtml-sqlvar user_id type="int">
-    <dtml-elif user>
-        username = <dtml-sqlvar user type="string">
     <dtml-else>
+        username = <dtml-sqlvar user type="string">
     </dtml-if>
